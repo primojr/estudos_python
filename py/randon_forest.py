53f7f5ceb29a0c795b275266d3b484909bf098b2
primojr <gersonprimo@gmail.com>
2022-08-02 17:34:48 -0300

    -- negados 2

--- py/randon_forest.py
+++ py/randon_forest.py
@@ -2,3 +2,23 @@
 ## Aplicar randon forest 
 
 import pandas as pd
+import matplotlib as pl
+import seaborn as sns
+
+df = pd.read_csv("date/WA_Fn-UseC_-Telco-Customer-Churn.csv")
+
+df.head()
+
+# Y
+df['Churn'].value_counts()
+
+df['Churn'].value_counts(normalize = True)*100
+
+df.MonthlyCharges.hist()
+
+
+ax = sns.boxplot(x = 'MonthlyCharges', y = 'Churn', data = df, orient = 'h')
+ax.figure.set_size_inches(12, 4)
+ax.set_title('Renda', fontsize = 18)
+ax.set_xlabel('R$', fontsize = 14)
+ax
